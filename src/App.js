182589a23ab10decc2feea69c9925b35a9d4b157
Isabel Louise Vicente <isabellouiseph@gmail.com>
2024-01-23 23:24:50 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+import BlogDetails from "./pages/BlogDetails";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<BrowserRouter>
+				<div className="App">
+					<Navbar />
+				</div>
+				<div className="main">
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/create" element={<Create />} />
+						<Route path="/blogs/:id" element={<BlogDetails />} />
+					</Routes>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/Create.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Create.css";
+
+const Create = () => {
+	const [title, setTitle] = useState("");
+	const [body, setBody] = useState("");
+	const [author, setAuthor] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+	const navigate = useNavigate();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const blog = { title, body, author };
+
+		setIsLoading(true);
+
+		fetch("http://localhost:8000/blogs/", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(blog),
+		}).then(() => {
+			console.log("new blog added");
+			setIsLoading(false);
+			navigate("/");
+		});
+	};
+
+	return (
+		<div className="create">
+			<h2>Add a New Blog</h2>
+			<form onSubmit={handleSubmit}>
+				<label>Blog Title: </label>
+				<input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
+				<label>Blog body: </label>
+				<textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+				<label>Blog author: </label>
+				<input type="text" required value={author} onChange={(e) => setAuthor(e.target.value)} />
+				{!isLoading && <button>Add Blog</button>}
+				{isLoading && <button disabled>Adding Blog...</button>}
+			</form>
+		</div>
+	);
+};
+
+export default Create;

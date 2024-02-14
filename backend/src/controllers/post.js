afcec32910c553cc0487cf139f9f81bbb49b8f0b
Isabel Louise Vicente <isabellouiseph@gmail.com>
2024-02-14 15:59:28 +0800

    edited backend and added new frontend codes

--- backend/src/controllers/post.js
+++ backend/src/controllers/post.js
@@ -22,12 +22,9 @@ const createPost = async (req, res) => {
 const getAllPosts = async (req, res) => {
 	// res.json({ mssg: "Get all posts" });
 	try {
-		const posts = await Post.find();
+		const posts = await Post.find({});
 
-		res.status(200).json({
-			count: posts.length,
-			posts,
-		});
+		res.status(200).json(posts);
 	} catch (error) {
 		res.status(400).json({ error: error.message });
 	}
@@ -38,7 +35,7 @@ const getPost = async (req, res) => {
 	const { id } = req.params;
 
 	try {
-		const post = await Post.findById({ _id: id });
+		const post = await Post.findById(id);
 		if (!post) return res.status(404).json({ error: "No post found" });
 
 		res.status(200).json(post);

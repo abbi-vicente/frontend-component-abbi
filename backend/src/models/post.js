a19969a26f395862eebb4bb35e16c0dbb71c9d43
Isabel Louise Vicente <isabellouiseph@gmail.com>
2024-02-15 18:59:18 +0800

    edited backend and made some edits in frontend

--- backend/src/models/post.js
+++ backend/src/models/post.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    author: { type: String, required: true },
-    description: { type: String, required: true },
-    likes: { type: Number, required: true },
-    comments: [{ body: String }],
+	title: { type: String, required: true },
+	author: { type: String, required: true },
+	description: { type: String, required: true },
+	likes: { type: Number },
+	comments: [{ body: String }],
 });
 
 const Post = mongoose.model("Post", postSchema);
